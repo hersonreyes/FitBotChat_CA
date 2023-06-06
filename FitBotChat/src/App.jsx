5f0431487c3504d05dedd487e4d66b0e5134d7a5
Alberto <alberto101481@gmail.com>
2023-06-06 15:43:59 -0600

    :sparkles: Add: AuthContext and AuthProvider

--- FitBotChat/src/App.jsx
+++ FitBotChat/src/App.jsx
@@ -1,11 +1,12 @@
+import { AuthProvider } from './auth/AuthContext';
 import AppRouter from './router/AppRouter';
 
 function App() {
 
   return (
-    <div className="App">
+    <AuthProvider>
       <AppRouter />
-    </div>
+    </AuthProvider>
   )
 }
 
