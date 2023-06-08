563403de0ed3a4deb57514500dc931782c8f225c
Alberto <alberto101481@gmail.com>
2023-06-08 10:34:53 -0600

    :sparkles: Add: client connection and Disconnection socket

--- FitBotChat/src/context/SocketContext.jsx
+++ FitBotChat/src/context/SocketContext.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
 import { createContext } from 'react';
 import { useSocket } from '../hooks/useSocket';
+import { AuthContext } from '../auth/AuthContext';
 
 export const SocketContext = createContext();
 
 
 export const SocketProvider = ({ children }) => {
 
-    const { socket, online } = useSocket('http://localhost:8080');
+    const { socket, online, connectSocket, disconnectSocket } = useSocket('http://localhost:8080');
+    const { auth } = useContext( AuthContext );
+
+    useEffect(() => {
+
+        if(auth.logged){
+            connectSocket();
+        }
+
+    }, [auth, connectSocket]);
+
+    useEffect(() => {
+
+        if(!auth.logged){
+            disconnectSocket();
+        }
+
+    }, [auth, disconnectSocket]);
     
     return (
         <SocketContext.Provider value={{ socket, online }}>
