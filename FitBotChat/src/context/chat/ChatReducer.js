dc51d526a1fec3dad42ac1404958ea211617b3ff
Alberto <alberto101481@gmail.com>
2023-06-09 17:01:24 -0600

    :sparkles: Add: Load users to context feature

--- FitBotChat/src/context/chat/ChatReducer.js
+++ FitBotChat/src/context/chat/ChatReducer.js
@@ -1,7 +1,16 @@
+import { types } from "../../types/Types";
 
 export const chatReducer = (state, action) => {
+    
     switch(action.type) {
+        case types.uploadedUsers:
+            return{
+                ...state,
+                users: [ ...action.payload ]
+            }
+
         default:
             return state;
     }
+
 }
---
+++ FitBotChat/src/types/Types.js
@@ -0,0 +1,6 @@
+
+export const types = {
+
+    uploadedUsers: '[Chat] Uploaded users',
+
+}
