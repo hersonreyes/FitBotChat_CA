2f4959dbaa5b0291fcf0244f1f34e18e3b0393d0
Alberto <alberto101481@gmail.com>
2023-06-09 16:22:09 -0600

    :sparkles: Add: Chat context, provider and reducer

--- FitBotChat/src/App.jsx
+++ FitBotChat/src/App.jsx
@@ -1,15 +1,18 @@
 import { AuthProvider } from './auth/AuthContext';
 import { SocketProvider } from './context/SocketContext';
+import { ChatProvider } from './context/chat/ChatContext';
 import AppRouter from './router/AppRouter';
 
 function App() {
 
   return (
-    <AuthProvider>
-      <SocketProvider>
-        <AppRouter />
-      </SocketProvider>
-    </AuthProvider>
+    <ChatProvider >
+      <AuthProvider>
+        <SocketProvider>
+          <AppRouter />
+        </SocketProvider>
+      </AuthProvider>
+    </ChatProvider>
   )
 }
 
