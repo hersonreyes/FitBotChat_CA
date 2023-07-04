0ed8706bcdf25aa16a92be9265e42a85f15eb21d
MJZF00155619 <00155619@uca.edu.sv>
2023-07-03 22:32:44 -0600

    Adding Commets SideBar and Home

--- FitBotChat/src/components/Sidebar.jsx
+++ FitBotChat/src/components/Sidebar.jsx
@@ -5,13 +5,16 @@ import { ChatContext } from '../context/chat/ChatContext';
 import { AuthContext } from '../auth/AuthContext';
 
 const Sidebar = () => {
-
+  // Obtiene el estado del chat y la información de autenticación del contexto
   const { chatState } = useContext(ChatContext);
   const { auth } = useContext(AuthContext);
 
   return (
     <div className="inbox_chat">
+        {/* Componente de la barra lateral del bot */}
         <SideBarBot />
+
+        {/* Renderiza los elementos de chat de la barra lateral para los usuarios */}
         {
             chatState.users
                 .filter( user => user.uid !== auth.uid )
@@ -23,11 +26,11 @@ const Sidebar = () => {
             ))
         }
 
-        {/* <!-- Espacio extra para scroll --> */}
+        {/* Espacio adicional para el desplazamiento */}
         <div className="extra_space"></div>
 
     </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
--- FitBotChat/src/pages/Home.jsx
+++ FitBotChat/src/pages/Home.jsx
@@ -1,42 +1,43 @@
-import React, { useContext, useEffect } from 'react';
-import '../styles/Chat.css';
-import InboxPeople from '../components/InboxPeople';
-import Messages from '../components/Messages';
-import ChatSelect from '../components/ChatSelect';
-import { useNavigate } from 'react-router-dom';
-import { ChatContext } from '../context/chat/ChatContext';
-import BotMessages from '../components/BotMessages';
+import React, { useContext, useEffect } from "react";
+import "../styles/Chat.css";
+import InboxPeople from "../components/InboxPeople";
+import Messages from "../components/Messages";
+import ChatSelect from "../components/ChatSelect";
+import { useNavigate } from "react-router-dom";
+import { ChatContext } from "../context/chat/ChatContext";
+import BotMessages from "../components/BotMessages";
 
 const Home = () => {
-
-  const { chatState } = useContext( ChatContext );
+  // Obtiene el estado del chat del contexto ChatContext
+  const { chatState } = useContext(ChatContext);
 
   let navigate = useNavigate();
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
-    if(!token){
+    // Si no hay un token de autenticación, redirige al usuario a la página de inicio de sesión
+    if (!token) {
       return navigate("/auth/login");
     }
   }, [token]);
 
   return (
     <div className="messaging">
-        <div className="inbox_msg">
+      <div className="inbox_msg">
+        {/* Componente de la lista de personas en el chat */}
+        <InboxPeople />
 
-            <InboxPeople />
-
-            {
-                (chatState.activeChat)
-                    ? <Messages />
-                    : (chatState.activeBot) ? <BotMessages />
-                    :  <ChatSelect />
-            }
-        </div>
-
-
+        {/* Renderiza el componente de mensajes si hay un chat activo */}
+        {chatState.activeChat ? (
+          <Messages />
+        ) : chatState.activeBot ? (
+          <BotMessages />
+        ) : (
+          <ChatSelect />
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
